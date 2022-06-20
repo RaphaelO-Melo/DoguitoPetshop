8a236a0ec4463432e3997ba9380c39589ceb8966
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-06-20 20:48:09 -0300

    First Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 
-function App() {
-  return (
-  <>
-    <Home />
-    <Sobre />
-  </>
-  )
-}
+export default function App() {
 
-export default App
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </Router>
+  );
+
+}
