f8524fdd848a6108b2b0a60e7ec41e3426a6786f
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-06-21 19:42:15 -0300

    Page404 component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './assets/css/base/base.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Pagina404 />} />
       </Routes>
     </Router>
   );
