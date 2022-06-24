c9809519eb149ba66fc1cb0302fb42431daad066
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-06-24 19:36:19 -0300

    Category buttons

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ListaPost from '../assets/components/ListaPost'
+import React from 'react';
+import ListaPost from '../assets/components/ListaPost';
+import ListaCategorias from './ListaCategorias';
 
-const Home = () => {
-
+export default function Home() {
   return (
     <main>
       <div className="container">
         <h2 className="titulo-pagina">Pet notícias</h2>
       </div>
+      <ListaCategorias />
       <ListaPost url={"/posts"} />
     </main>
   )
 }
-
-export default Home
